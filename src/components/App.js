18a5fe27573317a8dbeaca423a81bd0d61ef3116
nareshbo <nareshbo@microsoft.com>
2021-10-13 23:34:18 +0530

    route fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import CreateImage from "./CreateImage";
 import UploadImage from "./UploadImage";
 import Mint from "./Mint";
-import {Route, Router, Switch } from "react-router-dom";
+import {Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 export const history = createBrowserHistory();
@@ -10,7 +10,7 @@ function App () {
     return (
 <Router history={history}>
     <Switch>
-        <Route path={"/createImage"} component={CreateImage} />
+        <Route path={"/"} exact component={CreateImage} />
         <Route path="/upload" component={UploadImage} />        
         <Route path="/mint" component={Mint} />
     </Switch>
