3a2d5a9d8e171d7a8e146d23caec6b049ec649b3
Piyush Dubey <piyushdubey@microsoft.com>
2021-10-13 00:58:50 -0700

    Add minting logic

--- src/contracts/Picasso.sol
+++ src/contracts/Picasso.sol
@@ -3,7 +3,22 @@ pragma solidity ^0.5.16;
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 
 contract Picasso is ERC721Full {
+
+    string[] public imageUrls;
+    mapping(string => bool) _imageExists;
+
     constructor() ERC721Full("Picasso", "PICASSO") public {
 
+    }
+
+    function mint(string memory _imageUrl) public {
+        // Require unique image
+        uint _id = 0;//generate unique guid
+        
+        // Image - add it
+        // Call the mint function
+        _mint(msg.sender, _id);
+        _imageExists[_imageUrl] = true;
+        // Image - track it
     }
 }
