7944f4bbc258b5f529ac2a865583d816e0b54e14
Piyush Dubey <piyushdubey@microsoft.com>
2021-10-13 00:30:17 -0700

    Add test cases

--- test/Picasso.test.js
+++ test/Picasso.test.js
@@ -1,3 +1,4 @@
+const { assert } = require('chai');
 const { unstable_concurrentAct } = require('react-dom/test-utils');
 
 const Picasso = artifacts.require('./Picasso.sol');
@@ -6,10 +7,31 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-unstable_concurrentAct('Picasso', (accounts) => {
+contract('Picasso', (accounts) => {
+    let contract
+
+    before(async () => {
+        contract = await Picasso.deployed()
+    })
+
     describe('deployment', async() => {
         it('deploys successfully', async() => {
-            
+            const address = contract.address
+            console.log(address);
+            assert.notEqual(address, '')
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('has a name', async() => {
+            const name = await contract.name()
+            assert.equal(name, 'Picasso')
+        })
+
+        it('has a symbol', async() => {
+            const symbol = await contract.symbol()
+            assert.equal(symbol, 'PICASSO')
         })
     })
 })
